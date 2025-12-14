73ea84835b636bbf6908ce62c9c70371797d4dee
Tommischiri <tommischiri@gmail.com>
2025-12-14 21:13:10 +0100

    Invert queue and play next gestures option

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,6 +160,10 @@ android {
             initWith(getByName("release"))
             signingConfig = signingConfigs.getByName("persistentDebug")
         }
+        create("staging") {
+            initWith(getByName("release"))
+            signingConfig = signingConfigs.getByName("persistentDebug")
+        }
     }
 
     compileOptions {
