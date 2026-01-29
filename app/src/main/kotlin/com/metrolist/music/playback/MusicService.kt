76074937a036ee05141456621a5c02d8110454f0
Tommischiri <tommischiri@gmail.com>
2026-01-29 16:01:18 +0100

    Spotify Like queue feature

--- app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
+++ app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
@@ -425,6 +425,8 @@ class MusicService :
 
     private var spotifyLikeQueue: Boolean = true
 
+    private var spotifyLikeQueue: Boolean = true
+
     override fun onCreate() {
         super.onCreate()
         spotifyLikeQueue = dataStore.get(SpotifyLikeQueueKey, true)
