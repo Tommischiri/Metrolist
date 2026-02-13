3dc87fd34b3368065b1e57e71db8be8b25ef8ed5
Tommischiri <tommischiri@gmail.com>
2026-02-13 12:42:11 +0100

    renamed vars

--- app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
@@ -110,7 +110,7 @@ fun PlayerSettings(
         CrossfadeGaplessKey,
         defaultValue = true
     )
-    val (spotifyLikeQueue, onSpotifyLikeQueue) = rememberPreference(
+    val (spotifyLikeQueue, onSpotifyLikeQueueChange) = rememberPreference(
         SpotifyLikeQueueKey,
         defaultValue = true
     )
@@ -692,7 +692,7 @@ fun PlayerSettings(
                     trailingContent = {
                         Switch(
                             checked = spotifyLikeQueue,
-                            onCheckedChange = onSpotifyLikeQueue,
+                            onCheckedChange = onSpotifyLikeQueueChange,
                             thumbContent = {
                                 Icon(
                                     painter = painterResource(
@@ -704,7 +704,7 @@ fun PlayerSettings(
                             }
                         )
                     },
-                    onClick = { onSpotifyLikeQueue(!spotifyLikeQueue) }
+                    onClick = { onSpotifyLikeQueueChange(!spotifyLikeQueue) }
                 ),
                 Material3SettingsItem(
                     icon = painterResource(R.drawable.queue_music),
