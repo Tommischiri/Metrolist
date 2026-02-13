47e9d96c5fc9ff7a7fed269f8bf5f179e37b61a0
Tommischiri <tommischiri@gmail.com>
2026-02-13 12:49:10 +0100

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,10 +160,6 @@ android {
             initWith(getByName("release"))
             signingConfig = signingConfigs.getByName("persistentDebug")
         }
-        create("staging") {
-            initWith(getByName("release"))
-            signingConfig = signingConfigs.getByName("persistentDebug")
-        }
     }
 
     compileOptions {
