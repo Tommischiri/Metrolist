183766cc71c82c7d9d6646f900ea23fe45388ba4
Tommischiri <tommischiri@gmail.com>
2026-02-13 12:42:11 +0100

    renamed vars

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 @file:Suppress("UnstableApiUsage")
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
