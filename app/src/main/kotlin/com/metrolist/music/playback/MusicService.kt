c5db40f5a23d2b90d30336165b148a863de9ea17
Tommischiri <tommischiri@gmail.com>
2026-02-20 20:04:40 +0100

    o

--- app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
+++ app/src/main/kotlin/com/metrolist/music/playback/MusicService.kt
@@ -486,7 +486,6 @@ class MusicService :
 
     private var spotifyLikeQueue: Boolean = true
 
-    private var spotifyLikeQueue: Boolean = true
 
     override fun onCreate() {
         super.onCreate()
