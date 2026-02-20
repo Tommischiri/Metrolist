5acf80c5c695ab448f354100285699134040cce7
Tommischiri <tommischiri@gmail.com>
2026-02-20 22:42:47 +0100

    idk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -159,6 +159,7 @@ android {
         create("staging") {
             initWith(getByName("release"))
             signingConfig = signingConfigs.getByName("persistentDebug")
+            matchingFallbacks += listOf("release", "debug")
         }
     }
 
