bea044147dd4ea7f785c8afc0ae60692ec8cc42b
Tommischiri <tommischiri@gmail.com>
2026-02-20 20:18:24 +0100

    git is trash

--- app/src/main/kotlin/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/settings/AppearanceSettings.kt
@@ -267,11 +267,6 @@ fun AppearanceSettings(
         defaultValue = false
     )
 
-    val (betterSwipeToSong, onBetterSwipeToSongChange) = rememberPreference(
-        BetterSwipeToSongKey,
-        defaultValue = false
-    )
-
     val (invertSwipeToSong, onInvertSwipeToSongChange) = rememberPreference(
         InvertSwipeToSongKey,
         defaultValue = false
